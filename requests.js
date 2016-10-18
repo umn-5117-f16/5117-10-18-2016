c74fc6667f53b46d58673f0a0273f28fc3fd9ff5
Max Harper <maxharp3r@gmail.com>
2016-10-17 20:46:33 -0500

    add node.js example

--- requests.js
+++ requests.js
@@ -1,19 +1,48 @@
 
-// external lib
+// alternative to http
 var request = require('request');
 
-var options = {
-  url: 'https://api.github.com/repos/request/request',
-  headers: {
-    'User-Agent': 'request'
-  }
-};
+// support cookies on filesystem
+var FileCookieStore = require('tough-cookie-filestore');
+var j = request.jar(new FileCookieStore('cookies.json'));
 
-request.post(
-  'https://movielens.org/api/sessions',
-  {form:{userName:'a', password: 'a'}},
-  function(err, httpResponse, body) {
-    console.log(err)
-    console.log(httpResponse)
+request = request.defaults({
+  jar: j,
+  baseUrl: 'https://movielens.org'
+})
+
+var httpCallback = function callback(error, response, body) {
+  if (!error && response.statusCode == 200) {
+    var info = JSON.parse(body);
+    console.log(JSON.stringify(info, null, 2))
+  } else {
+    console.log("failed... %s", response.statusCode)
     console.log(body)
-  })
+  }
+}
+
+var api = {
+
+  login: function() {
+    request.post({
+      url: '/api/sessions',
+      headers: {
+        'content-type': 'application/json',
+        'User-Agent': 'request'
+      },
+      body: JSON.stringify({userName: '', password: ''})
+    }, httpCallback)
+  },
+
+  logout: function() {
+    request.delete({url: '/api/sessions/me'}, httpCallback)
+  },
+
+  movie: function(movieId) {
+    request.get('/api/movies/' + movieId, httpCallback)
+  }
+}
+
+// api.login()
+// api.movie(1)
+api.logout();
